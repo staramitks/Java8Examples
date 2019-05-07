a664b3f5c785a171d81369c6f427b4ad61618856
Amit Kumar Singh <star.amitks@gmail.com>
2019-05-07 14:23:46 +0530

    Create AccountTransaction.java
    Class for capturing transaction details of account

---
+++ src/main/java/home/amit/java/basic/executor/AccountTransaction.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package home.amit.java.basic.executor;
+
+/**
+ * @author singami2
+ *
+ */
+public class AccountTransaction {
+	
+	private Account account;
+	
+	public AccountTransaction(Account acnt)
+	{
+		this.account=acnt;
+	}
+
+	
+	public synchronized void withdraw ( long amount )
+	{
+		
+		 if (account.getBalance()<amount)
+		 {
+			 
+			 System.out.println("Less balance so holding withdrawal now...");
+			try{
+			 this.wait();
+			}
+			 catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			
+		 }
+		
+			System.out.println("Allowing withdrawal now...");
+			account.setBalance(account.getBalance()-amount);
+			notifyAll();
+	}
+	
+	public synchronized void deposit ( long amount )
+	{
+		
+		System.out.println("Depositing money and notifying other waiting threads ");
+		account.setBalance(account.getBalance()+amount);
+		notifyAll();
+		
+	}
+	
+	
+	public synchronized double getBalance ( )
+	{
+		
+		return account.getBalance(); 
+		
+		
+	}
+}
