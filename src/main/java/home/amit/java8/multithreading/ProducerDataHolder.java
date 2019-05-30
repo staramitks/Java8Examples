a10b09c3ac13b1a3e73ad69d176be8598250974b
Amit Kumar Singh <star.amitks@gmail.com>
2019-05-30 15:33:07 +0530

    Create ProducerDataHolder.java

---
+++ src/main/java/home/amit/java8/multithreading/ProducerDataHolder.java
@@ -0,0 +1,31 @@
+package home.amit.java.basic.multithreading;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author singami2
+ *
+ */
+public class ProducerDataHolder {
+	
+	
+	private BlockingQueue<Integer> dataHolder= new ArrayBlockingQueue<>(1);
+	
+	
+	public ProducerDataHolder(BlockingQueue<Integer> blockingQueue)
+	{
+		this.setDataHolder(blockingQueue);
+	}
+
+
+	public BlockingQueue<Integer> getDataHolder() {
+		return dataHolder;
+	}
+
+
+	public void setDataHolder(BlockingQueue<Integer> dataHolder) {
+		this.dataHolder = dataHolder;
+	}
+
+}
